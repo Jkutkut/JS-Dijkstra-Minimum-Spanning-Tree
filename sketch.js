461dd4d44d27528015f4b7ff0c9553787b25bc4a
jkutkut <jorgeregonzalez@gmail.com>
2021-04-08 10:46:36 +0200

    basic logic to generate the nodes added

--- sketch.js
+++ sketch.js
@@ -1,4 +1,18 @@
 var mainCanvasHeight, mainCanvasWidth;
+const ELEM = 12;
+const R = 200;
+
+const SHAPE = [
+    {R: 100, ELEM: 6},
+    {R: 200, ELEM: 3},
+    {R: 300, ELEM: 12}
+
+]
+
+
+var nodes;
+const NODESIZE = 50;
+
 
 function setup() {
     mainCanvasWidth = windowWidth;
@@ -10,11 +24,44 @@ function setup() {
     }
 
     createCanvas(mainCanvasWidth, mainCanvasHeight);
+
+
+    let center = createVector(mainCanvasWidth / 2, mainCanvasHeight / 2);
+
+
+
+    nodes = [];
+
+    nodes.push(new Node(center, 0, NODESIZE));
+
+
+    // let deltaTheta = 2 * Math.PI / ELEM;
+    let angle, pos;
+    let index = 0;
+
+    for (let lvl of SHAPE) {
+        for (let deltaTheta = 0; deltaTheta < Math.PI * 2; deltaTheta += 2 * Math.PI / lvl.ELEM) {
+            angle = 0 + deltaTheta;
+
+            pos = createVector(lvl.R * Math.cos(angle), lvl.R * Math.sin(angle));
+
+            pos.add(center);
+
+            nodes.push(new Node(pos, index++, NODESIZE))
+            console.log("node created")
+        }
+    }
+
+
 }
 
 
 
 function draw() {
     background(color(200));
+
+    for (let node of nodes) {
+        node.show();
+    }
 
 }
