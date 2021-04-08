563e896979d9815a215bbdcc9d3a57e8ffd04754
jkutkut <jorgeregonzalez@gmail.com>
2021-04-08 13:23:32 +0200

    arrow drawing updated

--- Classes/arch.js
+++ Classes/arch.js
@@ -8,14 +8,27 @@ class Arch {
         this.angle = atan2(this.origin.pos.y - this.destination.pos.y, this.origin.pos.x - this.destination.pos.x); //gets the angle of the line
 
         this.arrowTipSize = this.destination.size / 4;
+
+        // let offset = this.origin.pos.copy().sub(this.destination.pos).heading();
+        let offsetV = createVector(1, 0);
+        offsetV.rotate(this.angle);
+        this.lineOffset = {
+            start: offsetV.copy().mult(this.origin.size * 0.5),
+            end: offsetV.copy().mult(this.destination.size * 0.5)
+        }
     }
 
     show() {
         push();
-            line(this.origin.pos.x, this.origin.pos.y, this.destination.pos.x, this.destination.pos.y)
+            line(
+                this.origin.pos.x - this.lineOffset.start.x,
+                this.origin.pos.y - this.lineOffset.start.y,
+                this.destination.pos.x + this.lineOffset.end.x,
+                this.destination.pos.y + this.lineOffset.end.y
+            )
             translate(this.origin.pos.x, this.origin.pos.y); //translates to the destination vertex
             push();
-                rotate(this.angle);
+                rotate(this.angle - PI);
                 translate(this.distancia);
                 // console.log(this.distancia)
                 beginShape();
