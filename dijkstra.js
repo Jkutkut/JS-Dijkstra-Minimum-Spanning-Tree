8d6d49930dfa051f2df98a2b1315c8d68b73c20b
jkutkut <jorgeregonzalez@gmail.com>
2021-04-08 21:40:14 +0200

    pseudo code added

--- dijkstra.js
+++ dijkstra.js
@@ -1,6 +1,11 @@
 function* dijkstra() {
     i = 0;
+    selectedNodes = set(nodes[0]);
+    focusedNode = nodes[0];
     while (true) {
+        // search new mates and set them to selected
+        // search min cost and set that node to valid
+        // repeat until no more mates
         yield i++;
     }
 }
